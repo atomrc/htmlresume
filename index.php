18bc37ad33c4a5e61d15ad81eb4e594dfac35a32
Thomas Belin <ThomasBelin4@gmail.com>
2011-09-30 19:28:40 +0200

    Replacing Google Analytics by piwik

--- index.php
+++ index.php
@@ -7,21 +7,18 @@
 		<script type="text/javascript" src="js/general.js"></script>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		
-		
+		<!-- Piwik -->
 		<script type="text/javascript">
-
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-26000718-1']);
-		  _gaq.push(['_setDomainName', 'thomasbelin.fr']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-
-		</script>
+		var pkBaseURL = (("https:" == document.location.protocol) ? "https://cv.thomasbelin.fr/piwik/" : "http://cv.thomasbelin.fr/piwik/");
+		document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+		</script><script type="text/javascript">
+		try {
+		var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+		piwikTracker.trackPageView();
+		piwikTracker.enableLinkTracking();
+		} catch( err ) {}
+		</script><noscript><p><img src="http://cv.thomasbelin.fr/piwik/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+		<!-- End Piwik Tracking Code -->
 		
 		<script>
 			window.onload = initCV;
